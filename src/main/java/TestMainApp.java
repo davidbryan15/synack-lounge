a6a6bbbbbe5c0ab1d199bc4e809cafc406976625
davidbryan <aviles.davidb@gmail.com>
2021-12-07 10:10:31 -0500

    gitignore added and Java Mail Functionality

--- src/main/java/TestMainApp.java
+++ src/main/java/TestMainApp.java
@@ -3,22 +3,22 @@ import com.aviles.entity.Product;
 import com.aviles.entity.User;
 import com.aviles.service.EmfService;
 import com.aviles.util.CredentialsDbUtil;
+import com.aviles.util.MailUtil;
 import com.aviles.util.ProductDBUtil;
 import com.aviles.util.UserDbUtil;
 
+import javax.mail.MessagingException;
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 import java.util.List;
 
-public class MainApp {
+public class TestMainApp {
+
     public static void main(String[] args) {
+
+
+ // this portion of the app is intended to provide some testing capabilites
         System.out.println("begin");
-
-        EntityManager emf = EmfService.getEntityManagerFactory().createEntityManager();
-
-        List<Product> products = ProductDBUtil.getProducts();
-
-        System.out.println("Products: " + products.toString());
 
         System.out.println("the end");
     }
